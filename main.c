0b3deb8792c6f32dcf640061c1cf200b97dbcc76
Oualid Obbad <oobbad@c1r4p5.1337.ma>
2025-09-23 16:41:34 +0100

    good

--- main.c
+++ main.c
@@ -19,12 +19,8 @@ typedef struct s_info
 #define MAP_NUM_COLS 15
 #define TILE_SIZE 64
 #define MOVE_SPEED 10
-#define ROTATION_SPEED 0.1
+#define ROTATION_SPEED 0.2
 
-#define KEY_W 119
-#define KEY_S 115
-#define KEY_A 97
-#define KEY_D 100
 #define KEY_LEFT 65361
 #define KEY_RIGHT 65363
 #define KEY_UP 65362
@@ -58,35 +54,34 @@ int arr[MAP_NUM_ROWS][MAP_NUM_COLS] = {
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 
-// function castAllRays(){
-// 	var columndId = 0;
-// 	var rayAngle = player.rotationAngel - (FOV_ANGLE / 2);
-// 	rays = [];
-// 	for (var i = 0; i <NUM_RAYS; i++){
-// 		var ray = new Ray(rayAngle);
-// 		rays.push(ray);
-// 		rayAngle += FOV_ANGLE / NUM_RAYS;
-// 		columndId++;
-// 	}
-// }
+void *img_p;
+char *img_s;
+int bpp, size_of_one_row, endian;
 
-void rect(t_info *info, int x, int y, int color, int tile_size){
+void set_color(int x, int y,int color)
+{
+    // if (x >= WINDOW_WIDTH || y >= WINDOW_HEIGHT)
+    //     return ;
 
-	
+    int *row = (int *)(img_s + y * size_of_one_row);
+    row[x] = color;
+}
+
+void rect(int x, int y, int color, int tile_size){
+
 	int i = x;
 	while(i < x + tile_size)
 	{
 		int j = y;
 		while(j < y + tile_size){
-			mlx_pixel_put(info->mlx, info->win, i , j, color);
+			set_color(i, j, color);
 			j++;
 		}
 		i++;
 	}
 }
 
-
-void    line(t_info *info, int x0, int y0, int x1, int y1, int color)
+void    line(int x0, int y0, int x1, int y1, int color)
 {
     int dx = abs(x1 - x0);
     int dy = abs(y1 - y0);
@@ -96,7 +91,7 @@ void    line(t_info *info, int x0, int y0, int x1, int y1, int color)
 
     while (1)
     {
-        mlx_pixel_put(info->mlx, info->win, x0, y0, color);
+        set_color(x0, y0, color);
         if (x0 == x1 && y0 == y1)
             break;
         int e2 = 2 * err;
@@ -112,42 +107,43 @@ void    line(t_info *info, int x0, int y0, int x1, int y1, int color)
         }
     }
 }
-void castRays(t_info *info, int x, int y){
-	
+
+void castRays(int x, int y){
+
 	double rayAngle = rotationAngle - (FOV_ANGLE / 2);
 	for (int i = 0; i < NUM_RAYS; i++){
 
-		int line_end_x = x + (int)(cos(rayAngle) * 30);
-    	int line_end_y = y + (int)(sin(rayAngle) * 30);
-		line(info, x, y, line_end_x, line_end_y, 0xff0000);
+		int line_end_x = x + (int)(cos(rayAngle) * 60);
+    	int line_end_y = y + (int)(sin(rayAngle) * 60);
+		line(x, y, line_end_x, line_end_y, 0xff0000);
 		rayAngle += FOV_ANGLE / NUM_RAYS;
 	}
-
 }
+
 void render(t_info *info)
 {
-	
 	for (int i = 0; i < MAP_NUM_ROWS; i++){
 		for (int j = 0; j < MAP_NUM_COLS; j++){
 			if (arr[i][j] == 1){
-				rect(info, j * 64, i * 64, 0x000000, TILE_SIZE);
+				rect(j * TILE_SIZE, i * TILE_SIZE, 0x000000, TILE_SIZE);
 			}
-			else if (arr[i][j] == 0)
-				rect(info,  j * 64, i * 64, 0xffffff, TILE_SIZE);
+			else if (arr[i][j] == 0){
+				rect(j * TILE_SIZE, i * TILE_SIZE, 0xffffff, TILE_SIZE);
+			}
 		}
 	}
-	rect(info, x_p - 10, y_p - 10, 0xff0000, 20);
+	rect(x_p - 10, y_p - 10, 0xff0000, 20);
 	int line_end_x = x_p + (int)(cos(rotationAngle) * 30);
     int line_end_y = y_p + (int)(sin(rotationAngle) * 30);
-    line(info, x_p, y_p, line_end_x, line_end_y, 0xff0000);
-	castRays(info, x_p, y_p);
+    line( x_p, y_p, line_end_x, line_end_y, 0xff0000);
+	castRays(x_p, y_p);
+	mlx_put_image_to_window(info->mlx, info->win, img_p, 0, 0);
 }
 
 int is_validDer(int x, int y){
-	x /= TILE_SIZE;
-	y /= TILE_SIZE;
-	x = floor(x);
-	y = floor(y);
+
+	x = floor(x / TILE_SIZE);
+	y = floor(y / TILE_SIZE);
 
 	if (arr[y][x] == 1){
 		return 0;
@@ -178,6 +174,7 @@ int movement(int key, t_info *info){
 
 	int x = x_p + cos(rotationAngle) * moveStep;
 	int y = y_p + sin(rotationAngle) * moveStep;
+
 	if (is_validDer(x, y)){
 		x_p = x;
 		y_p = y;
@@ -189,13 +186,12 @@ int movement(int key, t_info *info){
 int main()
 {
 	t_info info;
-	int width;
-	int hieght;
 
 	info.mlx = mlx_init();
+	img_p = mlx_new_image(info.mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	img_s = mlx_get_data_addr(img_p, &bpp, &size_of_one_row, &endian);
+
 	info.win = mlx_new_window(info.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "title");
-	info.img_floor = mlx_xpm_file_to_image(info.mlx, "texture/floor.xpm", &width, &hieght);
-	info.img_wall = mlx_xpm_file_to_image(info.mlx, "texture/wall.xpm", &width, &hieght);
 	render(&info);
 	mlx_hook(info.win, 2, 1L<<0, movement, &info);
 	mlx_loop(info.mlx);
